df252d59e8c4d99799172511ec2072200901d2d8
F O'Brien <42175565+F-OBrien@users.noreply.github.com>
2022-06-09 23:29:39 +0100

    Add sort optoins for Tokens Assigned chart

--- components/charts/operatorCharts/OperatorsTokensAssigned.tsx
+++ components/charts/operatorCharts/OperatorsTokensAssigned.tsx
@@ -1,34 +1,59 @@
 import { useRef, useEffect, useState, useMemo } from 'react';
-import { Chart as ChartJS, CategoryScale, LinearScale, LogarithmicScale, BarElement, Title, Tooltip, Legend, ChartData } from 'chart.js';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartData,
+  PointElement,
+  LineElement,
+} from 'chart.js';
 import zoomPlugin from 'chartjs-plugin-zoom';
 import { Chart } from 'react-chartjs-2';
 import { operatorsNames } from '../../../constants/constants';
 import Spinner, { MiniSpinner } from '../../Spinner';
 import { useSdk } from '../../../hooks/useSdk';
-import { useEraStakers } from '../../../hooks/StakingQueries';
+import { useEraPreferences, useEraStakers } from '../../../hooks/StakingQueries';
 import { useStakingContext } from '../../../hooks/useStakingContext';
+import { Balance } from '@polkadot/types/interfaces';
+import { useEraTotalStaked } from '../../../hooks/StakingQueries';
+import { BigNumber } from '@polymathnetwork/polymesh-sdk';
 
-ChartJS.register(CategoryScale, LinearScale, LogarithmicScale, BarElement, Title, Tooltip, Legend, zoomPlugin);
+ChartJS.register(CategoryScale, LinearScale, LogarithmicScale, BarElement, PointElement, LineElement, Title, Tooltip, Legend, zoomPlugin);
 
 interface AssignedData {
-  labels?: string[];
-  data?: number[];
-  nominatingCount?: number[];
+  operator: string;
+  namedOperator: string;
+  totalAssigned: number;
+  nominatingCount: number;
+  commission: number;
+  aprAfterCommission: number;
 }
+[];
 
 const OperatorsTokensAssigned = () => {
   const {
+    api,
     chainData: { tokenDecimals, tokenSymbol },
     encodedSelectedAddress,
   } = useSdk();
   const divisor = 10 ** tokenDecimals;
   const {
     eraInfo: { currentEra },
+    stakingConstants: { fixedYearlyReward, maxVariableInflationTotalIssuance },
   } = useStakingContext();
 
   const [chartData, setChartData] = useState<ChartData<'bar' | 'line'>>();
-  const [assignedData, setAssignedData] = useState<AssignedData>({});
+  const [assignedData, setAssignedData] = useState<AssignedData[]>([]);
   const currentEraStakingData = useEraStakers(currentEra, { staleTime: Infinity });
+  const [totalIssuance, setTotalIssuance] = useState<Balance>();
+  const currentEraTotalStaked = useEraTotalStaked(currentEra, { staleTime: Infinity });
+  const currentEraPreferences = useEraPreferences(currentEra, { staleTime: Infinity });
+  const [sortMode, setSortMode] = useState<number>(0);
 
   // Define reference for tracking mounted state
   const mountedRef = useRef(false);
@@ -46,6 +71,12 @@ const OperatorsTokensAssigned = () => {
     chartRef.current?.resetZoom();
   };
 
+  const toggleSort = () => {
+    sortMode < 5 ? setSortMode(sortMode + 1) : setSortMode(0);
+  };
+
+  const sortModeEnum = ['APR', 'Tokens Assigned', 'Active Nominations', 'Commission', 'Address', 'Name'];
+
   const chartOptions = useMemo(() => {
     const options = {
       responsive: true,
@@ -53,7 +84,6 @@ const OperatorsTokensAssigned = () => {
         y: {
           beginAtZero: true,
           title: { display: true, text: `Amount [${tokenSymbol}]` },
-          // type: 'logarithmic' as const,
         },
         y1: {
           display: false,
@@ -64,10 +94,19 @@ const OperatorsTokensAssigned = () => {
             display: false,
           },
         },
+        y2: {
+          display: true,
+          beginAtZero: true,
+          title: { display: true, text: `Percent [%]` },
+          position: 'right' as const,
+          grid: {
+            display: false,
+          },
+        },
       },
       plugins: {
         legend: {
-          display: false,
+          display: true, //false,
           //   position: 'top' as const,
         },
         title: {
@@ -90,74 +129,143 @@ const OperatorsTokensAssigned = () => {
     return options;
   }, [currentEra, tokenSymbol]);
 
+  // Subscribe to Total POLYX Issuance.
+  useEffect(() => {
+    if (!api.query.balances) {
+      return;
+    }
+    let unsubPolyxSupply: () => void;
+
+    const getTotalIssuance = async () => {
+      unsubPolyxSupply = await api.query.balances.totalIssuance((total) => {
+        setTotalIssuance(total);
+      });
+    };
+
+    getTotalIssuance();
+
+    return () => {
+      unsubPolyxSupply && unsubPolyxSupply();
+    };
+  }, [api.isConnected, api.query.balances]);
+
+  // Calculate annual total reward from Total, issuance and Total Staked.
+  const annualTotalReward = useMemo(() => {
+    if (!totalIssuance || !currentEraTotalStaked.data) return;
+
+    // TODO consider making reward curve a reusable function
+    const xIdeal = 0.7; // Ideal Staked ratio.
+    const iIdeal = 0.14; // Inflation at ideal staked ratio.
+    const iZero = 0.025; // Inflation at staked ratio = 0%.
+    const decay = 0.05; // decay for staked ratio greater than the ideal
+
+    const stakedRatio = currentEraTotalStaked.data.toNumber() / totalIssuance?.toNumber();
+
+    let inflation;
+
+    if (totalIssuance?.toNumber() < maxVariableInflationTotalIssuance.toNumber()) {
+      if (stakedRatio <= xIdeal) {
+        inflation = iZero + (iIdeal - iZero) * (stakedRatio / xIdeal);
+      } else {
+        inflation = iZero + (iIdeal - iZero) * 2 ** ((xIdeal - stakedRatio) / decay);
+      }
+    } else {
+      inflation = fixedYearlyReward.toNumber() / totalIssuance?.toNumber();
+    }
+
+    const annualTotalReward = (inflation * totalIssuance?.toNumber()) / divisor;
+
+    return annualTotalReward;
+  }, [totalIssuance, currentEraTotalStaked.data, maxVariableInflationTotalIssuance, divisor, fixedYearlyReward]);
+
   // Set `dataIsFetching` to true while any of the queries are fetching.
   const dataIsFetching = useMemo(() => {
-    return currentEraStakingData.isFetching;
-  }, [currentEraStakingData.isFetching]);
+    return currentEraStakingData.isFetching || currentEraTotalStaked.isFetching;
+  }, [currentEraStakingData.isFetching, currentEraTotalStaked.isFetching]);
 
   useEffect(() => {
-    if (!currentEraStakingData.data) return;
+    if (
+      !currentEraStakingData.data ||
+      !annualTotalReward ||
+      !currentEraPreferences.data ||
+      currentEraStakingData.data.era.toNumber() !== currentEraPreferences.data.era.toNumber()
+    )
+      return;
 
-    let labels: string[] = [];
-    let data: number[] = [];
-    let nominatingCount: number[] = [];
-    let pos: number;
+    let unsortedData: AssignedData[] = [];
 
     Object.entries(currentEraStakingData.data.operators).forEach(([operator, { total, others }]) => {
       const totalAssigned = total.unwrap().toNumber() / divisor;
-      // Sort from highest to Lowest
-      // If there is nothing in the array add to first position.
-      if (data.length === 0) {
-        data.push(totalAssigned);
-        pos = 0;
-      }
-      // Perform a binary search to find correct position in the sort order.
-      else {
-        let start = 0;
-        let end = data.length - 1;
-        pos = 0;
-
-        while (start <= end) {
-          var mid = start + Math.floor((end - start) / 2);
-          // If the amount is the same as mid position position we can add at the mid +1 position.
-          if (totalAssigned === data[mid]) {
-            pos = mid + 1;
-            break;
-            // If the amount is greater than the mid position it should be before mid.
-          } else if (totalAssigned > data[mid]) {
-            pos = end = mid - 1;
-          } else {
-            //  If the amount is less than than the mid position it should be after it mid.
-            pos = start = mid + 1;
-          }
-          // Once the end of the search is reached the position should be the final "start"
-          // This ensures the new data is not inserted at a position of mid -1 which would
-          // place it out of order.
-          if (start > end) {
-            pos = start;
-          }
-        }
-        data.splice(pos, 0, totalAssigned);
-      }
-
-      labels.splice(pos, 0, operator.toString());
-      nominatingCount.splice(pos, 0, others.length);
+      const operatorCount = Object.keys(currentEraStakingData.data.operators).length;
+      const nominatingCount = others.length;
+      const commission = new BigNumber(currentEraPreferences.data?.operators[operator.toString()].commission.toString()).div(10_000_000).toNumber();
+      const aprAfterCommission = ((100 - commission) * annualTotalReward) / operatorCount / totalAssigned;
+      const namedOperator = operatorsNames[operator]
+        ? operatorsNames[operator]
+        : operator.slice(0, 5) + '...' + operator.slice(operator.length - 5, operator.length);
+      unsortedData.push({ operator, namedOperator, totalAssigned, nominatingCount, commission, aprAfterCommission });
     });
 
     if (mountedRef.current) {
-      setAssignedData({ labels, data, nominatingCount });
+      setAssignedData(unsortedData);
     }
-  }, [currentEraStakingData.data, divisor]);
+  }, [annualTotalReward, currentEraPreferences.data, currentEraStakingData.data, divisor]);
 
   useEffect(() => {
-    if (!assignedData.labels || !assignedData.data || !assignedData.nominatingCount) return;
+    if (assignedData.length === 0) return;
 
-    const { labels, data, nominatingCount } = assignedData;
+    // Sort the array from highest to lowest points
+    assignedData.sort((a, b) => {
+      switch (sortMode) {
+        case 0:
+          return b.aprAfterCommission - a.aprAfterCommission;
+        case 1:
+          return b.totalAssigned - a.totalAssigned;
+        case 2:
+          return b.nominatingCount - a.nominatingCount;
+        case 3:
+          return a.commission - b.commission;
+        case 4:
+          const addressA = a.operator.toUpperCase(); // ignore upper and lowercase
+          const addressB = b.operator.toUpperCase(); // ignore upper and lowercase
+          if (addressA < addressB) {
+            return -1;
+          }
+          if (addressA > addressB) {
+            return 1;
+          }
+          // names must be equal
+          return 0;
+        case 5:
+          const nameA = a.namedOperator.toUpperCase(); // ignore upper and lowercase
+          const nameB = b.namedOperator.toUpperCase(); // ignore upper and lowercase
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          // names must be equal
+          return 0;
+        default:
+          return 0;
+      }
+    });
 
     let bgcolor: any[] = [];
     let bdcolor: any[] = [];
     let namedLabels: string[] = [];
-    labels.forEach((operator, index) => {
+    let totalsAssigned: number[] = [];
+    let nominatingCounts: number[] = [];
+    let apr: number[] = [];
+    let commissions: number[] = [];
+
+    assignedData.forEach(({ operator, namedOperator, totalAssigned, nominatingCount, commission, aprAfterCommission }, index) => {
+      totalsAssigned[index] = totalAssigned;
+      nominatingCounts[index] = nominatingCount;
+      commissions[index] = commission;
+      apr[index] = aprAfterCommission;
+      namedLabels[index] = namedOperator;
       if (
         encodedSelectedAddress &&
         currentEraStakingData.data?.nominators[encodedSelectedAddress]?.some((nominated) => nominated.operator === operator)
@@ -168,11 +276,6 @@ const OperatorsTokensAssigned = () => {
         bdcolor[index] = '#EC4673';
         bgcolor[index] = '#EC467395';
       }
-
-      // Assign Operator name if available
-      namedLabels[index] = operatorsNames[operator]
-        ? operatorsNames[operator]
-        : operator.slice(0, 5) + '...' + operator.slice(operator.length - 5, operator.length);
     });
 
     const assignedTokensChartData = {
@@ -181,16 +284,34 @@ const OperatorsTokensAssigned = () => {
         {
           type: 'line' as const,
           label: 'Active Nominations',
-          data: nominatingCount,
+          data: nominatingCounts,
           backgroundColor: '#43195B95',
           borderColor: '#43195B',
           borderWidth: 2,
           yAxisID: 'y1',
         },
+        {
+          type: 'line' as const,
+          label: 'Estimated APR',
+          data: apr,
+          backgroundColor: 'rgba(35,87,49,0.5)',
+          borderColor: 'rgba(35,87,49,1)',
+          borderWidth: 2,
+          yAxisID: 'y2',
+        },
+        {
+          type: 'line' as const,
+          label: 'Operator Commission',
+          data: commissions,
+          backgroundColor: 'rgba(150,10,10,0.5)',
+          borderColor: 'rgba(150,10,10,1)',
+          borderWidth: 2,
+          yAxisID: 'y2',
+        },
 
         {
           label: 'Assigned',
-          data: data,
+          data: totalsAssigned,
           backgroundColor: bgcolor,
           borderColor: bdcolor,
           borderWidth: 2,
@@ -202,7 +323,7 @@ const OperatorsTokensAssigned = () => {
     if (mountedRef.current) {
       setChartData(assignedTokensChartData);
     }
-  }, [assignedData, currentEraStakingData.data?.nominators, encodedSelectedAddress]);
+  }, [assignedData, currentEraStakingData.data?.nominators, encodedSelectedAddress, sortMode]);
 
   return (
     <div className='LineChart'>
@@ -212,6 +333,10 @@ const OperatorsTokensAssigned = () => {
           <button className='resetZoomButton' onClick={resetChartZoom}>
             Reset Zoom
           </button>
+          <button className='sortToggle' onClick={toggleSort}>
+            Sorted by {sortModeEnum[sortMode]}
+          </button>
+
           {dataIsFetching ? <MiniSpinner /> : <></>}
         </>
       ) : (
