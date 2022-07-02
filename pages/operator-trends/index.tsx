1d7c68bb018775362617c2088a9e6c65da69338c
F O'Brien <42175565+F-OBrien@users.noreply.github.com>
2022-07-02 18:08:05 +0100

    update trend duration dropdown

--- pages/operator-trends/index.tsx
+++ pages/operator-trends/index.tsx
@@ -21,7 +21,9 @@ function App() {
             <option value='7'>7 eras</option>
             <option value='14'>14 eras</option>
             <option value='28'>28 eras</option>
-            <option value='52'>52 eras</option>
+            <option value='42'>42 eras</option>
+            <option value='56'>56 eras</option>
+            <option value='70'>70 eras</option>
             <option value='0'>Max </option>
           </select>
         </div>
