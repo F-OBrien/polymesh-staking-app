bd44b094874865f1208c0555c99f280fec4598b3
F-OBrien <fobcrypto@gmail.com>
2022-10-01 17:59:50 +0100

    attempt fix deployment

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-  basePath: '/polymesh-staking-app',
+  // basePath: '/polymesh-staking-app',
   images: {
     loader: 'akamai',
     path: './',
