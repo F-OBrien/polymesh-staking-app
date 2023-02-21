613ccce6dea0e7a70f74fe38da08cbac1840444c
F-OBrien <francis@polymesh.network>
2023-02-21 05:41:39 +0000

    apply next.js codemods

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
+import Image from "next/legacy/image";
 
 const Home: NextPage = () => {
   return (
